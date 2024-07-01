7f8b0ac34b7d78db79f0f4ec81192c044f15d5a3
Onat <onatklnc@gmail.com>
2024-07-02 00:54:59 +0300

    Initial Commit

---
+++ OnatCard/OnatCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnatCardApp.swift
+//  OnatCard
+//
+//  Created by Onat KILINÇ on 2.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct OnatCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
